1a7a941b42e52a1ccea13611d76e47bdc392c46b
Rafael Oliveira Souza <71654681+rafaoliveira22@users.noreply.github.com>
2022-10-12 19:13:23 -0300

    registrando,consultando e deletando utilizando o fast api

--- app/main.py
+++ app/main.py
@@ -1,49 +1,70 @@
+#libs
+import sqlite3
+from hashlib import sha256
+
 # FastAPI
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 
+# server
 import uvicorn
-from uuid import uuid4
 
 app = FastAPI()
 
-users = {0: 'Ingnave'}
+def db_commands(command):
+    con = sqlite3.connect('cadastro-user-fastapi-test.db')
+    cur = con.cursor()
+    try:
+        cur.execute('CREATE TABLE users(ID integer primary key autoincrement, NOME text ,CPF text);')
+    except: pass
+
+    # outros comandos
+    cur.execute(command)
+    con.commit()
+    con.close()
+
+def db_select(command):
+    con = sqlite3.connect('cadastro-user-fastapi-test.db')
+    cur = con.cursor()
+    cur.execute(command)
+    found = cur.fetchall()
+    return found
+
 # Cadastrar
-@app.get('/user/register/{user_register}', response_class = HTMLResponse)
-async def register(user_register: str):
-    if user_register:
-        id = str(uuid4())
-        users[id] = user_register
-        return f'<h1>{user_register} cadastrado com sucesso!!!</h1>{users}'
-    else:
-        return 'Status Code = ?????????'
+@app.post('/user/register/{name}-{cpf}', response_class = HTMLResponse, status_code = 201)
+async def register(name: str, cpf: str):
+    user = []
+    validationCPF = 0
+    shaCPF = ''
+    userName = name
+    userCPF = cpf
 
+    user.append(userName)
+    if len(userCPF) == 11 and userCPF.isnumeric():
+        for itemCPF in userCPF:
+            validationCPF += int(itemCPF)
+        if validationCPF == 44 or validationCPF == 55 or validationCPF == 66:
+            shaCPF = str(sha256(userCPF.encode('utf-8')).hexdigest())
+            user.append(shaCPF)
+    db_commands(f'INSERT INTO users(NOME, CPF) VALUES("{user[0]}", "{user[1]}");')
+    return f'<h1>{user[0]} cadastrado com sucesso</h1>'
 
-# Deletar
-@app.get('/user/delete/{user_delete}', response_class = HTMLResponse)
-async def delete(user_delete: str):
-    if user_delete:
-        return f'<h1>{users.get(user_delete)} excluido com sucesso!!!</h1>'
-        users.pop(user_delete)
+# Retornar - 1 usuário
+@app.get('/user/{_id}', response_class = HTMLResponse, status_code = 200)
+async def return_user(_id: int):
+    return f'<h1>{db_select(f"SELECT * FROM users where ID = {_id};")}</h1>'
 
-# Consultar um User
-@app.get('/user/{token_user}', response_class = HTMLResponse)
-async def return_user(token_user: str):
-    return f'<h1>{users.get(token_user)}</h1>'
-
-# Consultar todos
-@app.get('/user', response_class = HTMLResponse)
+# Retornar - Todos os usuários
+@app.get('/user', response_class = HTMLResponse, status_code = 200)
 async def return_users():
-    # users = users.json()
-    return f'<h1>{users}</h1>'
+    return f'<h1>{db_select("SELECT * FROM users;")}</h1>'
+
+# Deletar 1 usuário
+@app.delete('/user/delete/{_id}', response_class = HTMLResponse, status_code = 200)
+async def delete(_id: int):
+    name = db_select(f"SELECT NOME FROM users where ID = {_id};")[0]
+    db_commands(f"DELETE FROM users where ID = {_id}")
+    return f'<h1>{name} excluído com sucesso</h1>'
 
 if __name__ == '__main__':
 	uvicorn.run(app = 'main:app', reload = True)
-
-
-# TASKS
-# implementar um banco de dados
-# registrar,deletar e consultar no banco
-# mostrar o user ou os users em formato json
-# tratar os erros e pesquisar sobre os status_code
-
